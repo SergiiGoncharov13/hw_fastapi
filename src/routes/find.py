07bc0f050887c3b20bd789e2d103686f6151793a
SergiiGoncharov13 <goncharov.s.work@gmail.com>
2023-04-24 14:01:20 +0300

    Optimized  find.py

--- src/routes/find.py
+++ src/routes/find.py
@@ -1,6 +1,6 @@
-from typing import List
+from typing import Optional
 
-from fastapi import Depends, HTTPException, status, Path, APIRouter
+from fastapi import Depends, HTTPException, status, APIRouter, Query
 from sqlalchemy.orm import Session
 
 from src.database.db import get_db
@@ -12,46 +12,33 @@ from src.repository import find as repository_contacts
 find = APIRouter(prefix="/api/find", tags=['find'])
 
 
-@find.get("/firstname/{firstname}", response_model=UserResponse)
-async def get_user_firstname(firstname: str = Path(), db: Session = Depends(get_db),
-                             current_user: User = Depends(auth_service.get_current_user)):
-    contact = await repository_contacts.get_user_by_firstname(firstname, db)
+async def get_user_by_search_parameter(search_param: str, value: str, db: Session, current_user: User) -> Optional[
+    UserResponse]:
+    contact = None
+    if search_param == "firstname":
+        contact = await repository_contacts.get_user_by_firstname(value, db)
+    elif search_param == "lastname":
+        contact = await repository_contacts.get_user_by_lastname(value, db)
+    elif search_param == "email":
+        contact = await repository_contacts.get_user_by_email(value, db)
+    elif search_param == "phone":
+        contact = await repository_contacts.get_user_by_phone(value, db)
+
     if contact is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Not Found")
     return contact
 
 
-@find.get("/lastname/{lastname}", response_model=UserResponse)
-async def get_user_lastname(lastname: str = Path(), db: Session = Depends(get_db),
-                            current_user: User = Depends(auth_service.get_current_user)):
-    contact = await repository_contacts.get_user_by_lastname(lastname, db)
-    if contact is None:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Not Found")
-    return contact
-
-
-@find.get("/email/{email}", response_model=UserResponse)
-async def get_user_email(email: str = Path(), db: Session = Depends(get_db),
+@find.get("/{search_param}")
+async def search_contact(search_param: str, value: str = Query(..., min_length=1), db: Session = Depends(get_db),
                          current_user: User = Depends(auth_service.get_current_user)):
-    contact = await repository_contacts.get_user_by_email(email, db)
-    if contact is None:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Not Found")
+    contact = await get_user_by_search_parameter(search_param, value, db, current_user)
     return contact
 
 
-@find.get("/phone/{phone}", response_model=UserResponse)
-async def get_user_email(phone: str = Path(), db: Session = Depends(get_db),
-                         current_user: User = Depends(auth_service.get_current_user)):
-    contact = await repository_contacts.get_user_by_phone(phone, db)
-    if contact is None:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Not Found")
-    return contact
-
-
-@find.get("/shift/{shift}", response_model=List[UserResponse])
-async def get_birthday_list(shift: int, db: Session = Depends(get_db),
+@find.get("/birthday_list")
+async def get_birthday_list(shift: int = Query(0),
+                            db: Session = Depends(get_db),
                             current_user: User = Depends(auth_service.get_current_user)):
     contacts = await repository_contacts.get_birthday_list(shift, db)
-    if contacts is None:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Not Found")
     return contacts
